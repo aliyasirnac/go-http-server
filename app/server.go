a5388b28b1f2702a26cad30c94aedc14f07f2315
Ali Yasir Naç <76746351+aliyasirnac@users.noreply.github.com>
2024-07-18 23:13:27 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -19,9 +19,11 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	con, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	t, err := con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	fmt.Println(t)
 }
