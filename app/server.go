df5264a08e4a6fd37b4f64588e3de4b2fa8c0d20
Ali Yasir Naç <76746351+aliyasirnac@users.noreply.github.com>
2024-07-18 23:31:09 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"fmt"
-	// Uncomment this block to pass the first stage
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	//
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -20,10 +17,19 @@ func main() {
 	}
 
 	con, err := l.Accept()
+
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
-	t, err := con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-	fmt.Println(t)
+
+	req := make([]byte, 1024)
+	con.Read(req)
+	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
+		con.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		con.Close()
+		return
+	}
+
+	con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 }
