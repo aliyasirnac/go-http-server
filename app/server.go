8682b384aaef79d6739323ab20a2320c98fb1f7b
Ali Yasir Naç <76746351+aliyasirnac@users.noreply.github.com>
2024-07-19 11:56:02 +0300

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -24,12 +24,25 @@ func main() {
 	}
 
 	req := make([]byte, 1024)
-	con.Read(req)
-	if !strings.HasPrefix(string(req), "GET / HTTP/1.1") {
-		con.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	n, err := con.Read(req)
+	if err != nil {
+		return
+	}
+	s := string(req[:n])
+	path := strings.Split(s, " ")[1]
+	if path == "/" {
+		con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		con.Close()
 		return
 	}
 
-	con.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	if strings.Split(path, "/")[1] == "echo" {
+		message := strings.Split(path, "/")[2]
+		res := fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length:%v\r\n\r\n%s", len(message), message)
+		con.Write([]byte(res))
+		return
+	}
+
+	con.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	con.Close()
 }
